c804e88ccea8ac9e53806a272bfc1e30597cf832
teng-oh <juntengoh@gmail.com>
2020-09-28 14:43:33 +0800

    1

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -60,9 +60,9 @@ from Train_Utils import (
 
 keras_path = os.path.join(src_path, "keras_yolo3")
 Data_Folder = os.path.join(get_parent_dir(1), "Data")
-Image_Folder = os.path.join(Data_Folder, "Source_Images", "Training_Images")
+Image_Folder = "kaggle/input/image-rec-training-dataset/Training_Images"
 VoTT_Folder = os.path.join(Image_Folder, "vott-csv-export")
-YOLO_filename = os.path.join(VoTT_Folder, "data_train.txt")
+YOLO_filename = "kaggle/input/image-rec-training-dataset/Training_Images"
 
 Model_Folder = os.path.join(Data_Folder, "Model_Weights")
 YOLO_classname = os.path.join(Model_Folder, "data_classes.txt")
